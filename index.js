1dc0be5661c77fc192e2d640194f4c1ef736624d
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2021-04-03 00:39:42 -0500

    Segunda entrada

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server:server });
 const port = 3000
+
+app.use(express.static(__dirname + "/views"));
+
+wss.on('connection', function connection(ws) {
+    console.log('A new client Connected!');
+    ws.send('Bienvenido!!');
+    enviarHora();
+});
+
+function enviarHora(){
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var seg = fecha.getSeconds();
+    wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(hora + " : " + minutos + " : " + seg);
+        }
+    });
+    var upd = setTimeout('enviarHora()' , 500);
+}
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
---
+++ views/reloj.js
@@ -0,0 +1,17 @@
+
+function conectarAServidor() {
+    var socket = new WebSocket('ws://192.168.0.16:3000' , 'echo-protocol');
+
+    //Abro la coneccion
+    socket.addEventListener('open', function (event) {
+        console.log('Connected to WS Server');
+        document.getElementById('Reloj').innerHTML = "Conectado al servidor";
+    });
+    
+    // Espero mensajes
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+        document.getElementById('Reloj').innerHTML = "" + event.data;
+    });
+
+}
